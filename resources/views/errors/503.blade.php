094520009f6718616c86ce30bdfa3f5007b63d6a
Jonny Anderson <jonnyanderson@Apollo-II.home>
2017-05-02 16:31:46 -0600

    Update 503 error page

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -40,7 +40,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Be right back.</div>
+                <div class="title">Artisan Gaming is down for maintenance.</div>
             </div>
         </div>
     </body>
